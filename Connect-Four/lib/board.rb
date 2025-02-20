48303aefe062a085a3c890d187c93a0575d5b933
TMSD <dias.tiago03@gmail.com>
2025-02-20 16:36:07 +0000

    defined place and col_full funcs

--- Connect-Four/lib/board.rb
+++ Connect-Four/lib/board.rb
@@ -11,4 +11,21 @@ class Board
       blue: '🔵'
     }
   end
+
+  def col_full?(col)
+    @board.all? { |row| !row[col].nil? }
+  end
+
+  def place(col, color)
+    return 'Invalid Column' unless (0...7).cover?(col)
+
+    return 'Column is full' if col_full?(col)
+
+    row = 0
+    (0..5).each do |i|
+      row = i
+      break if @board[i][col].nil?
+    end
+    @board[row][col] = @symbols[color]
+  end
 end
--- Connect-Four/spec/connect_spec.rb
+++ Connect-Four/spec/connect_spec.rb
@@ -12,27 +12,36 @@ describe Board do
     end
   end
 
+  describe '#col_full?' do
+    it 'returns if the column has empty spaces' do
+      board.place(3, :red)
+      expect(board.col_full?(3)).to be false
+      6.times { board.place(1, :blue) }
+      exepct(col_full?(1)).to be true
+    end
+  end
+
   describe '#place' do
     it 'placing a piece in the bottom row' do
       board.place(3, :red)
-      expect(board.board[6][2]).to eq('🔴')
+      expect(board.board[5][3]).to eq('🔴')
     end
 
     it 'placing a piece stacked on top of the first one' do
       board.place(3, :red)
       board.place(3, :blue)
-      expect(board.board[6][2]).to eq('🔴')
-      expect(board.board[5][2]).to eq('🔵')
+      expect(board.board[5][3]).to eq('🔴')
+      expect(board.board[4][3]).to eq('🔵')
     end
 
     it 'placing a piece in a full column' do
       6.times { board.place(1, :red) }
-      expect { board.place(1, :red) }.to raise_error('Column is full')
+      expect(board.place(1, :red)).to eq('Column is full')
     end
 
     it 'placing a piece in an invalid column' do
-      expect { board.place(-1, :red) }.to raise_error('Invalid Column')
-      expect { board.place(6, :red) }.to raise_error('Invalid Column')
+      expect(board.place(-1, :red)).to eq('Invalid Column')
+      expect(board.place(7, :red)).to eq('Invalid Column')
     end
   end
 end
