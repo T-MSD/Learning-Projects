f92fdc9d6baab52e8462f6a4858e9a2920a3a86b
T-MSD <dias.tiago03@gmail.com>
2025-02-20 14:43:23 +0000

    place tests

--- Connect-Four/lib/board.rb
+++ Connect-Four/lib/board.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
+# Board Class
 class Board
-  
+  attr_reader :board, :symbols
+
+  def initialize
+    @board = Array.new(6) { Array.new(7, nil) }
+    @symbols = {
+      red: '🔴',
+      blue: '🔵'
+    }
+  end
 end
--- Connect-Four/spec/connect_spec.rb
+++ Connect-Four/spec/connect_spec.rb
@@ -5,10 +5,34 @@ require_relative '../lib/board'
 describe Board do
   board = Board.new
 
-  describe '.initialize' do
+  describe '#initialize' do
     it 'creates the board, 7x6 2d array' do
       expect(board.board).to be_an(Array)
-      expect(board.board).to eq([])
+      expect(board.board).to all(be_an(Array))
+    end
+  end
+
+  describe '#place' do
+    it 'placing a piece in the bottom row' do
+      board.place(3, :red)
+      expect(board.board[6][2]).to eq('🔴')
+    end
+
+    it 'placing a piece stacked on top of the first one' do
+      board.place(3, :red)
+      board.place(3, :blue)
+      expect(board.board[6][2]).to eq('🔴')
+      expect(board.board[5][2]).to eq('🔵')
+    end
+
+    it 'placing a piece in a full column' do
+      6.times { board.place(1, :red) }
+      expect { board.place(1, :red) }.to raise_error('Column is full')
+    end
+
+    it 'placing a piece in an invalid column' do
+      expect { board.place(-1, :red) }.to raise_error('Invalid Column')
+      expect { board.place(6, :red) }.to raise_error('Invalid Column')
     end
   end
 end
